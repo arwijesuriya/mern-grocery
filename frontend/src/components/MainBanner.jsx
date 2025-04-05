03055f60f1641920b61d0022ef8b32db5a732f87
Achira Wijesuriya <wijesuriya4me@gmail.com>
2025-04-06 03:01:34 +0530

    Update MainBanner.jsx

--- frontend/src/components/MainBanner.jsx
+++ frontend/src/components/MainBanner.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { assets } from '../assets/assets'
 
 const MainBanner = () => {
     return (
-        <div>
-            MainBanner
+        <div className="relative">
+            <img src={assets.main_banner_bg} alt="Banner" className="w-full hidden md:block" />
+            <img src={assets.main_banner_bg_sm} alt="Banner" className="w-full md:hidden" />
         </div>
     )
 }
