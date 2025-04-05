f8500f7ab9014039c3e204268a773e4c3c3a15fe
Achira Wijesuriya <wijesuriya4me@gmail.com>
2025-04-06 02:39:22 +0530

    logic for profile picture

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -35,9 +35,19 @@ const Navbar = () => {
                     <button className="absolute -top-2 -right-3 text-xs text-white bg-primary w-[18px] h-[18px] rounded-full">3</button>
                 </div>
 
-                <button className="cursor-pointer px-8 py-2 bg-primary hover:bg-primary-dull transition text-white rounded-full">
-                    Login
-                </button>
+                {!user ? (
+                    <button className="cursor-pointer px-8 py-2 bg-primary hover:bg-primary-dull transition text-white rounded-full">
+                        Login
+                    </button>
+                ) : (
+                    <div className="relative group">
+                        <img src={assets.profile_icon} className="w-10" alt="ProfileIcon" />
+                        <ul className="hidden group-hover:block absolute top-10 right-0 bg-white shadow border border-gray-200 py-2.5 w-30 rounded-md text-sm z-40">
+                            <li onClick={() => navigate("orders")} className="p-1.5 pl-3 hover:bg-primary/10 cursor-pointer">Orders</li>
+                            <li onClick={logout} className="p-1.5 pl-3 hover:bg-primary/10 cursor-pointer">Logout</li>
+                        </ul>
+                    </div>    
+                )}
             </div>
 
             {/* Mobile Menu */}
