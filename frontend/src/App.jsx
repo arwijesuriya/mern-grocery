e137ee5336b215deb925736376a6cbf4653081d2
Achira Wijesuriya <wijesuriya4me@gmail.com>
2025-04-06 03:52:39 +0530

    create banner dinal

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,7 +10,7 @@ const App = () => {
   return (
     <div>
       {isSellerPath ? null : <Navbar />}
-      <div className={`${isSellerPath ? "" : "px-6 md:px-16 lg:px-24 xl:px-32"}`}>
+      <div className={`${isSellerPath ? "" : "mt-10 px-6 md:px-16 lg:px-24 xl:px-32"}`}>
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
