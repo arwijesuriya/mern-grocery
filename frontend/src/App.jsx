bb9082ee5d73e7b8fdec5870b8ab9d2d314d42db
Achira Wijesuriya <wijesuriya4me@gmail.com>
2025-04-06 03:10:10 +0530

    add routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import Home from './pages/Home'
+import { Routes, Route } from 'react-router-dom'
 
 const App = () => {
   return (
     <div>
       <Navbar />
-      <Home />
+      <div className="px-6 md:px-16 lg:px-24 xl:px-32">
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
     </div>
   )
 }
