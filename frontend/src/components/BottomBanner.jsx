afdeb26cdf87ae6f46c7a32e5732e17cca14e80d
Achira Wijesuriya <wijesuriya4me@gmail.com>
2025-04-06 21:02:09 +0530

    Update BottomBanner.jsx

--- frontend/src/components/BottomBanner.jsx
+++ frontend/src/components/BottomBanner.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { assets } from '../assets/assets'
 
 const BottomBanner = () => {
-  return (
-    <div>BottomBanner</div>
-  )
+    return (
+        <div className="relative mt-24">
+            <img src={assets.bottom_banner_image} alt="BottomBanner" className="w-full hidden md:block" />
+            <img src={assets.bottom_banner_image_sm} alt="BottomBanner" className="w-full md:hidden" />
+        </div>
+    )
 }
 
 export default BottomBanner
