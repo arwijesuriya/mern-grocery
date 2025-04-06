52d5102aa585ab08643956266ef8d998beb76c08
Achira Wijesuriya <wijesuriya4me@gmail.com>
2025-04-06 13:29:13 +0530

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import MainBanner from '../components/MainBanner'
 import Categories from '../components/Categories'
+import BestSelller from '../components/BestSelller'
 
 const Home = () => {
     return (
         <div>
             <MainBanner />
             <Categories />
+            <BestSelller />
         </div>
     )
 }
