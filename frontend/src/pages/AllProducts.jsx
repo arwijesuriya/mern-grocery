29682506aec1381cdf0233af8e109fda6105c784
Achira Wijesuriya <wijesuriya4me@gmail.com>
2025-05-28 23:51:08 +0530

    frontend: AppContext.jsx useEffect

--- frontend/src/pages/AllProducts.jsx
+++ frontend/src/pages/AllProducts.jsx
@@ -1,9 +1,20 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useAppContext } from "../context/AppContext";
 
 const AllProducts = () => {
-    const { products } = useAppContext();
+    const { products, searchQuery } = useAppContext();
     const [filteredProducts, setFilteredProducts] = useState([]);
+
+    useEffect(() => {
+        if (searchQuery.length > 0) {
+            setFilteredProducts(products.filter(product => 
+                product.name.toLowerCase().includes(searchQuery.toLowerCase())
+            ));
+        }
+        else {
+            setFilteredProducts(products);
+        }
+    }, [products, searchQuery]);
 
     return (
         <div className="mt-16 flex flex-col">
