f0380559966040e23a03862b7d6eb9211f682eb5
Achira Wijesuriya <wijesuriya4me@gmail.com>
2025-05-28 23:25:21 +0530

    frontend: connect AllProducts.jsx with App.jsx

--- frontend/src/pages/AllProducts.jsx
+++ frontend/src/pages/AllProducts.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import { useAppContext } from "../context/AppContext";
 
 const AllProducts = () => {
-  return (
-    <div>AllProducts</div>
-  )
-}
+  const { products } = useAppContext();
 
-export default AllProducts
+    return (
+        <div className="mt-16 flex flex-col">
+            <div className="flex flex-col items-end w-max">
+                <p className="text-2xl font-medium uppercase">
+                    All products
+                </p>
+
+                <div className="w-16 h-0.5 bg-primary rounded-full">
+                    
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default AllProducts;
