9f22e3f58f4191f9f06a261b1349ce92ed82f77f
Achira Wijesuriya <wijesuriya4me@gmail.com>
2025-05-29 00:07:45 +0530

    frontend: Navbar.jsx onChange function

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -32,7 +32,14 @@ const Navbar = () => {
                 <NavLink to="/">Contact</NavLink>
 
                 <div className="hidden lg:flex items-center text-sm gap-2 border border-gray-300 px-3 rounded-full">
-                    <input className="py-1.5 w-full bg-transparent outline-none placeholder-gray-500" type="text" placeholder="Search products" />
+                    <input 
+                        onChange={(e) => 
+                            setSearchQuery(e.target.value)
+                        }
+                        className="py-1.5 w-full bg-transparent outline-none placeholder-gray-500" 
+                        type="text" 
+                        placeholder="Search products" 
+                    />
                     <img src={assets.search_icon} alt="Search" className="w-4 h-4" />
                 </div>
 
