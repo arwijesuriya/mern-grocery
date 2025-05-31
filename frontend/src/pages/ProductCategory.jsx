dc3c1858cd6470f9f79830de74b8b258e4cbc5d0
Achira Wijesuriya <wijesuriya4me@gmail.com>
2025-06-01 01:15:05 +0530

    frontend: ProductCategory.jsx updated

--- frontend/src/pages/ProductCategory.jsx
+++ frontend/src/pages/ProductCategory.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { useAppContext } from '../context/AppContext'
 import { useParams } from 'react-router-dom';
 import { categories } from '../assets/assets';
+import ProcuctCard from '../components/ProductCard';
 
 const ProductCategory = () => {
     const { products } = useAppContext();
@@ -21,6 +22,20 @@ const ProductCategory = () => {
                     <div className='w-16 h-0.5 bg-primary rounded-full'>
 
                     </div>
+                </div>
+            )}
+
+            { filteredProducts.length > 0 ? (
+                <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-5 gap-3 md:gap-6 mt-6'>
+                    {filteredProducts.map((product) => (
+                        <ProcuctCard key={product._id} product={product} />
+                    ))}
+                </div>
+            ) : (
+                <div className='flex items-center justify-center h-[60vh]'>
+                    <p className='text-2xl font-medium text-primary'>
+                        No products found in this category.
+                    </p>
                 </div>
             )}
         </div>
